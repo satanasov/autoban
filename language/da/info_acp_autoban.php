c805cfc18499aab8e30883ffa1dc002cd8c2b354
Anders Jenbo <anders@jenbo.dk>
2015-07-31 03:39:13 +0200

    Update translation

--- language/da/info_acp_autoban.php
+++ language/da/info_acp_autoban.php
@@ -26,6 +26,6 @@ $lang = array_merge($lang, array(
 	'AUTOBAN_COUNT_EXPLAIN'	=> 'Hvor mange advarsler kræves for automatisk udelukkelse',
 	'AUTOBAN_DURATION'	=> 'Varighed af automatisk udelukkelse',
 	'AUTOBAN_DURATION_EXPLAIN'	=> 'Varighed i dage for automatisk udelukkelse',
-	'AUTOBAN_REASON'	=> 'Ban årsag',
+	'AUTOBAN_REASON'	=> 'Årsag til udelukkelse',
 	'AUTOBAN_REASON_EXPLAIN'	=> 'Årsag til udelukkelse af bruger'
 ));
