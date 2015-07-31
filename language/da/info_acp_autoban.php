858ed08fba11666eaa797918bf25138de54cd8af
Anders Jenbo <anders@jenbo.dk>
2015-07-31 02:38:47 +0200

    Fix translation file

--- language/da/info_acp_autoban.php
+++ language/da/info_acp_autoban.php
@@ -21,11 +21,11 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'AUTOBAN_ACTIVE'	=> 'Automatisk udelukkelse',
-	'AUTOBAN_ACTIVE_EXPLAIN'	=> 'Aktivér automatisk udelukkelse på et defineret antal advarsler',"
+	'AUTOBAN_ACTIVE_EXPLAIN'	=> 'Aktivér automatisk udelukkelse på et defineret antal advarsler',
 	'AUTOBAN_COUNT'	=> 'Advarsels antal',
 	'AUTOBAN_COUNT_EXPLAIN'	=> 'Hvor mange advarsler kræves for automatisk udelukkelse',
 	'AUTOBAN_DURATION'	=> 'Varighed af automatisk udelukkelse',
-	'AUTOBAN_DURATION_EXPLAIN'	=> 'Varighes i dage for automatisk udelukkelse',
-	ic'AUTOBAN_REASON'	=> 'Ban årsag',
+	'AUTOBAN_DURATION_EXPLAIN'	=> 'Varighed i dage for automatisk udelukkelse',
+	'AUTOBAN_REASON'	=> 'Ban årsag',
 	'AUTOBAN_REASON_EXPLAIN'	=> 'Årsag til udelukkelse af bruger'
 ));
